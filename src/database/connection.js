b309b9608428dce4e05a9dc376a4c98e77dc9f0c
brandon <brandonsouza27@gmail.com>
2021-01-12 18:33:53 -0300

    Configuraçoes

--- src/database/connection.js
+++ src/database/connection.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose')
 
 mongoose.connect(process.env.MONGO_CONNECTION_URI, {
-	useNewUrlParser: true
+	useNewUrlParser: true,
+	useUnifiedTopology: true
 })
 
 module.exports = mongoose
