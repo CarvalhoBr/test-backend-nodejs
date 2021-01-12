a4c3fa3793322c6585a1bbaa359b3178a3eb3676
brandon <brandonsouza27@gmail.com>
2021-01-12 18:33:36 -0300

    Rota para busca de todos os produtos

--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,14 @@
 const routes = require('express').Router()
+const yup = require('yup')
+const Product = require('./models/Product')
+
+routes.get('/products', async (req, res) => {
+
+	const products = await Product.find({})
+	console.log(products)
+
+	return res.json(products)
+})
 
 routes.get('/', (req, res) => {
 	return res.json({Message: 'Hello from routes'})
